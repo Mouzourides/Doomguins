970060a6756195c97f274b39fd0da9d53069da1b
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2018-05-18 17:00:39 +0100

    Undoomify project

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,9 +4,7 @@ import {history} from '../../Modules/Store'
 
 import {
   DECREMENT,
-  INCREMENT,
-  GET_TITLE,
-  UPDATE_USER_TITLE
+  INCREMENT
 } from './HomeActionReducer'
 import Button from 'antd/es/button/button'
 
@@ -14,28 +12,8 @@ const Home = props => (
   <div>
     <div className='App'>
       <header className='App-header'>
-        <h1 className='App-title'>Welcome to Doomguins</h1>
-        <h4 className='App-title'>Doom Doom Doooooom</h4>
+        <h1 className='App-title'>react-boilerplate</h1>
       </header>
-      <pre className='App-intro'>
-        .___.<br />
-        /\ /\<br />
-        | O\_/O |<br />
-        / \_/ \<br />
-        .' / \ `.<br />
-        / _| |_ \<br />
-        (_/ | | \_)<br />
-        \ /<br />
-        __\_ _/__<br />
-        ~;/ \;~<br />
-      </pre>
-      <p>{props.title}</p>
-      <p>
-        <input type='text' onChange={(e) => props.updateUserTitle(e.currentTarget.value)} />
-        <Button type='primary' onClick={props.getTitle}>
-                DOOM
-        </Button>
-      </p>
 
       <p>Count: {props.count}</p>
 
@@ -61,8 +39,7 @@ const Home = props => (
 )
 
 const mapStateToProps = state => ({
-  count: state.counter.count,
-  title: state.counter.title
+  count: state.counter.count
 })
 
 const mapDispatchToProps = dispatch => {
@@ -75,17 +52,6 @@ const mapDispatchToProps = dispatch => {
     decrement: () => {
       dispatch({
         type: DECREMENT
-      })
-    },
-    getTitle: () => {
-      dispatch({
-        type: GET_TITLE
-      })
-    },
-    updateUserTitle: (value) => {
-      dispatch({
-        type: UPDATE_USER_TITLE,
-        payload: value
       })
     },
     changePage: () => history.push('/about-us')
--- src/Components/Home/HomeActionReducer.js
+++ src/Components/Home/HomeActionReducer.js
@@ -1,16 +1,12 @@
-import generateTitle from '../../utils/penguin-pop-culture-title-generator'
 
 // Actions
 export const INCREMENT = 'counter/INCREMENT'
 export const DECREMENT = 'counter/DECREMENT'
-export const GET_TITLE = 'counter/GET_TITLE'
-export const UPDATE_USER_TITLE = 'counter/UPDATE_USER_TITLE'
 
 // State
 const initialState = {
   count: 0,
-  userTitle: '',
-  title: generateTitle()
+  userTitle: ''
 }
 
 // Reducer
@@ -26,20 +22,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         count: state.count - 1
-      }
-
-    case UPDATE_USER_TITLE:
-      return {
-        ...state,
-        userTitle: action.payload
-      }
-
-    case GET_TITLE:
-      const tempTitle = state.userTitle ? [{ name: state.userTitle, type: 'movie' }] : undefined
-
-      return {
-        ...state,
-        title: generateTitle(tempTitle)
       }
 
     default:
