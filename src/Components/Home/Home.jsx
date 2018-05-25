55a10a79782eb56290e50234bead3f2aa55fbb58
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2018-05-25 08:20:59 +0100

    remove redux and routes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,58 +1,19 @@
-import React from "react";
-import { connect } from "react-redux";
-import { history } from "../../Config/Store";
-
-import { DECREMENT, INCREMENT } from "./HomeActionReducer";
+import React, { Component } from "react";
 import Button from "antd/es/button/button";
 
-const Home = props => (
-  <div>
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">react-boilerplate</h1>
-      </header>
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">react-boilerplate</h1>
+        </header>
+        <p>
+          <Button type="primary">Hello world</Button>
+        </p>
+      </div>
+    );
+  }
+}
 
-      <p>Count: {props.count}</p>
-
-      <p>
-        <Button type="primary" onClick={props.increment}>
-          Increment
-        </Button>
-      </p>
-
-      <p>
-        <Button type="primary" onClick={props.decrement}>
-          Decrement
-        </Button>
-      </p>
-
-      <p>
-        <Button type="primary" onClick={() => props.changePage()}>
-          Go to about page via redux
-        </Button>
-      </p>
-    </div>
-  </div>
-);
-
-const mapStateToProps = state => ({
-  count: state.counter.count
-});
-
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => {
-      dispatch({
-        type: INCREMENT
-      });
-    },
-    decrement: () => {
-      dispatch({
-        type: DECREMENT
-      });
-    },
-    changePage: () => history.push("/about-us")
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { ConnectedRouter } from "react-router-redux";
-import store, { history } from "./Config/Store";
-import App from "./Config/Routes";
+import Home from "./Components/Home/Home";
+import registerServiceWorker from "./Config/Store.js";
 
 import "sanitize.css/sanitize.css";
 import "./index.css";
 
-const target = document.querySelector("#root");
-
-render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  target
-);
+render(<Home />, document.getElementById("root"));
+registerServiceWorker();
