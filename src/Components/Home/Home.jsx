08bf97d3c2a4b8613f710b1552ccd58c28cd8117
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2018-05-25 17:50:31 +0100

    Add stateful component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,15 +2,38 @@ import React, { Component } from "react";
 import Button from "antd/es/button/button";
 
 class Home extends Component {
+  state = { count: 0 };
+
+  incrementCount() {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  }
+
+  decrementCount() {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">react-boilerplate</h1>
         </header>
-        <p>
-          <Button type="primary">Hello world</Button>
-        </p>
+        <div>
+          <p>
+            <b>Count:</b> {this.state.count}
+          </p>
+          <p>
+            <Button type="primary" onClick={() => this.incrementCount()}>
+              Increment
+            </Button>
+          </p>
+
+          <p>
+            <Button type="primary" onClick={() => this.decrementCount()}>
+              Decrement
+            </Button>
+          </p>
+        </div>
       </div>
     );
   }
